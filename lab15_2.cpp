22bba98f3313ad208a50a1e5ea918af97ddd813d
PunnawichMai <punnawich_dachin@cmu.ac.th>
2023-01-17 16:51:18 +0700

    finished

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,50 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double Mat[][N])
+{
+	for(int i = 0;i < N;i++)
+	{
+		cout << "Row " << i+1 << ": ";
+		for(int j = 0; j < N;j++)
+		{
+			cin >> Mat[i][j];
+		}
+	}
+}
+
+void showMatrix(const bool Mat[][N])
+{
+	for(int i = 0;i < N;i++)
+	{
+		for(int j = 0;j < N;j++)
+		{
+			cout << Mat[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
+
+void findLocalMax(const double QMat[][N], bool ansMat[][N])
+{
+	for(int i = 0;i <= N;i++)
+	{
+		for(int j = 0;j <= N;j++)
+		{
+			ansMat[i][j] = false;
+		} 
+	}
+	
+	for(int i = 1;i < N-1;i++)
+	{
+		for(int j = 1;j < N-1;j++)
+		{
+			if(QMat[i][j] >= QMat[i-1][j] and QMat[i][j] >= QMat[i+1][j] and QMat[i][j] >= QMat[i][j-1] and QMat[i][j] >= QMat[i][j+1])
+			{
+				ansMat[i][j] = true;
+			}
+		}
+	}
+}
+
